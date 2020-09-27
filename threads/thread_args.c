167b3675035fb3aaae6e58a8ae65bde54af8b3de
Gene <TinderholmGene@gmail.com>
2020-09-27 15:28:21 -0500

    add return value

--- threads/thread_args.c
+++ threads/thread_args.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 
 void* print_message(void* arg);
 
+#define NUM_THREADS 10
+
 int main (void) {
-    int indexes[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    pthread_t threads[10];
+    int indexes[NUM_THREADS] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    pthread_t threads[NUM_THREADS];
+    int* ret_vals[NUM_THREADS];
 
     printf("In main thread.\n");
 
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < NUM_THREADS; ++i) {
         pthread_create(&threads[i], NULL, print_message, &indexes[i]);
     }
-    for (int i = 0; i < 10; ++i) {
-        pthread_join(threads[i], NULL);
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        pthread_join(threads[i], (void**) &ret_vals[i]);
+    }
+
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        printf("Thread %2d returned %d\n", i + 1, *ret_vals[i]);
+        free(ret_vals[i]);
     }
 }
 
 void* print_message(void* arg) {
     int* thread_number = (int*) arg;
+    int* ret_val = malloc(sizeof(int));
 
-    printf("In thread %d.\n", *thread_number);
+    *ret_val = 10 - *thread_number;
 
-    return NULL;
+    printf("In thread %2d, returning %d.\n", *thread_number, *ret_val);
+
+    return ret_val;
 }
